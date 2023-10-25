6d89a410f762bc7082a092d97e0396c4851a16e4
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-10-25 11:28:24 +0300

    Add `dotenv` package
    - user config() method to load environment variables to process.env

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("http");
+require("dotenv").config();
 const app = require("./app");
 
 const port = 3000 || process.env.PORT;
 
 const server = http.createServer(app);
-
 server.listen(port, () => {
     console.log(`Server running at port ${port}...`);
 });
