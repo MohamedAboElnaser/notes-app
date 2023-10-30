88d4565be251d32797c21544c16329d0c40fca77
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-10-30 21:50:16 +0200

    Enhance the response back from signup for `testability`

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,11 +3,12 @@ const authService = require('../services/auth.service');
 
 const signup = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
-  await authService.signUp(name, email, password);
+  const otp = await authService.signUp(name, email, password);
 
   return res.status(200).json({
     status: 'success',
     message: `Check the confirmation otp code at ${email}`,
+    otp: process.env.NODE_ENV !== 'production' ? otp : null,
   });
 });
 
