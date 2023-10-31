54121e1d27b1c1b4d2967bcbb7184c26d34d8e2a
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-10-31 17:59:26 +0200

    Write `unit-test` for auth.service module

--- __test__/unit/auth.service.test.js
+++ __test__/unit/auth.service.test.js
@@ -10,6 +10,7 @@ jest.mock('../../config', () => ({
       findUnique: jest.fn(),
       create: jest.fn(),
       delete: jest.fn(),
+      findFirst: jest.fn(),
     },
     verification: {
       create: jest.fn(),
@@ -35,15 +36,12 @@ jest.mock('../../src/util/otp-service', () => ({
 }));
 
 describe('Test auth.service module  Unit-Testing', () => {
-
   afterEach(() => {
     jest.clearAllMocks();
   });
   describe('Test signUp ', () => {
     it('should throw an error if any field is missing', async () => {
-      await expect(AuthService.signUp()).rejects.toThrow(
-        'Missing required fields.',
-      );
+      await expect(AuthService.signUp()).rejects.toThrow();
     });
 
     it('should throw an error if the email is already registered', async () => {
@@ -70,11 +68,56 @@ describe('Test auth.service module  Unit-Testing', () => {
     });
     it('Should register a user', async () => {
       db.user.findUnique.mockReturnValue(null);
-      db.verification.create.mockResolvedValue({otp:'123456'});
+      db.verification.create.mockResolvedValue({ otp: '123456' });
       db.user.create.mockResolvedValue({ user: 'userName' });
       await expect(
         AuthService.signUp('user', 'user@gmail.com', 'password'),
       ).resolves.not.toThrow();
+    });
+  });
+
+  describe('Test login', () => {
+    it('Should throw error if any field is missing', async () => {
+      await expect(AuthService.login()).rejects.toThrow();
+    });
+
+    it('Should throw error if the email is not registered', async () => {
+      db.user.findFirst.mockReturnValue(null);
+      await expect(
+        AuthService.login('user@gmail.com', 'password'),
+      ).rejects.toThrow('This email is not attached to any user.');
+    });
+
+    it('Should throw error if the email is not activated', async () => {
+      db.user.findFirst.mockReturnValue({ isActive: false });
+      await expect(
+        AuthService.login('user@gmail.com', 'password'),
+      ).rejects.toThrow('You should activate your mail first.');
+    });
+
+    it('Should throw error if the password is wrong', async () => {
+      db.user.findFirst.mockReturnValue({
+        isActive: true,
+        password: 'test1234',
+      });
+      await expect(
+        AuthService.login('user@gmail.com', 'password'),
+      ).rejects.toThrow('Invalid Password');
+      expect(bcrypt.compare).toBeCalled();
+    });
+    //happy scenario
+    it('Should return jwt', async () => {
+      db.user.findFirst.mockReturnValue({
+        isActive: true,
+        password: 'password',
+      });
+      const returnedValue = await AuthService.login(
+        'user@gmail.com',
+        'password',
+      );
+      expect(returnedValue).toBe('jwt');
+      expect(JWTService.generate).toBeCalledTimes(1);
+      expect(bcrypt.compare).toBeCalledWith('password','password');
     });
   });
 });
