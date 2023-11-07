ecd05a0819447098956bd5a3a61fc6fb9a44b083
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-11-07 03:50:27 +0200

    fix `status code` returned from signUp controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ const signup = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
   const otp = await authService.signUp(name, email, password);
 
-  return res.status(200).json({
+  return res.status(201).json({
     status: 'success',
     message: `Check the confirmation otp code at ${email}`,
     otp: process.env.NODE_ENV !== 'production' ? otp : null,
