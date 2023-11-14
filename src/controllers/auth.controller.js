8524c78d9481624b32dd0a265cdbe32f4873a013
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-11-14 21:40:11 +0200

    Add `jsDoc` for `authController`

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,16 @@
 const { catchAsync } = require('../util');
 const authService = require('../services/auth.service');
-
+// /api/v1/auth/signup
+/**
+ * Handles user sign-up.
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @returns {Promise<void>} A promise that resolves once the response is sent.
+ * @throws {Error} If there is an issue during the sign-up process.
+ */
 const signup = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
   const otp = await authService.signUp(name, email, password);
@@ -11,7 +21,16 @@ const signup = catchAsync(async (req, res, next) => {
     otp: process.env.NODE_ENV !== 'production' ? otp : null,
   });
 });
-
+// /api/v1/auth/signup/verify-email
+/**
+ * Verifies user email using the provided OTP.
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Promise<void>} A promise that resolves once the response is sent.
+ * @throws {Error} If there is an issue during the email verification process.
+ */
 const verifyEmail = catchAsync(async (req, res) => {
   const { otp } = req.body;
   await authService.verifyEmail(otp);
@@ -21,7 +40,17 @@ const verifyEmail = catchAsync(async (req, res) => {
     message: 'Your email verified successfully, you can login now.',
   });
 });
-
+// /api/v1/auth/login
+/**
+ * Handles user login.
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @returns {Promise<void>} A promise that resolves once the response is sent.
+ * @throws {Error} If there is an issue during the login process.
+ */
 const login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
   const token = await authService.login(email, password);
@@ -37,7 +66,17 @@ const login = catchAsync(async (req, res, next) => {
     token,
   });
 });
-
+// /api/v1/auth/reverify-email
+/**
+ * Resend verification OTP to the user's email.
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @returns {Promise<void>} A promise that resolves once the response is sent.
+ * @throws {Error} If there is an issue during the OTP resend process.
+ */
 const reverifyEmail = catchAsync(async (req, res, next) => {
   const { email } = req.body;
   const otp = await authService.reverifyEmail(email);
