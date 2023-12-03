b4831b062b0de4e4d2694c6bda4e2f669a8c3b20
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-12-03 13:39:39 +0200

    delete previous migrations and migrate the last schema instead

--- prisma/migrations/20231023115706_add/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "notes" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
--- prisma/migrations/20231107035728_add_on_delete_constrain/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "verifications" DROP CONSTRAINT "verifications_userId_fkey";
-
--- AddForeignKey
-ALTER TABLE "verifications" ADD CONSTRAINT "verifications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
